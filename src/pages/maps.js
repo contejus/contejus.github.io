3f9e47b70c5a64912f9976184732df90126a530a
Tejus <tgmathew11@gmail.com>
2019-10-02 15:30:53 -0500

    added bootstrap navbar

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -4,6 +4,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import GoogleMapReact from 'google-map-react';
 import MapsApiKey from '../pages/api_keys';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {Navbar, NavbarBrand, Nav} from 'react-bootstrap';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
  
@@ -21,24 +24,30 @@ class SimpleMap extends Component {
     return (
       // Important! Always set the container height explicitly
       <React.Fragment>
-      <div style={{ backgroundColor: 'blue', height: '15vh', width: '100%'}}>
-          <table></table>
-      </div>
-      <div style={{ height: '85vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={
-                { key: this.props.key}
-            }
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
+        <Navbar bg='dark' variant='dark' style={{height: '10vh', width: '100%'}}>
+          <Navbar.Brand href="/">Tejus Mathew</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">Projects</Nav.Link>
+            <Nav.Link href="/">Visitor Map</Nav.Link>
+          </Nav>
+        </Navbar>
+        <div style={{ height: '90vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={
+                  { key: this.props.key}
+              }
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+        
       </React.Fragment>
     );
   }
