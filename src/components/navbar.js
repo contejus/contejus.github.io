27d508bea2517dc7cd7f695c91d644c7e79542bd
Tejus <tgmathew11@gmail.com>
2019-10-07 10:50:50 -0500

    modified navbar links, added projects page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,11 +5,11 @@ import {Navbar, NavbarBrand, Nav} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default () => 
-    <Navbar bg='dark' variant='dark' style={{height: '10vh', width: '100%'}}>
+  <Navbar bg='dark' variant='dark' style={{height: '10vh', width: '100%'}}>
     <Navbar.Brand href="/">Tejus Mathew</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/">Projects</Nav.Link>
-      <Nav.Link href="/">Visitor Map</Nav.Link>
+      <Nav.Link href="/projects">Projects</Nav.Link>
+      <Nav.Link href="/maps">Visitor Map</Nav.Link>
     </Nav>
   </Navbar>
