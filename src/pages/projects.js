34c1de3d4217573530c38d7dc7387ace88691146
Tejus <tgmathew11@gmail.com>
2019-10-09 10:10:18 -0500

    added navbar to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,14 +3,11 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {Navbar} from "../components/navbar"
 
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  <Navbar>
+  </Navbar>
 )
 
 export default SecondPage
