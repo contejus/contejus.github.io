6520a95801c150a0931e96d18feccccb45d86edb
Tejus <tgmathew11@gmail.com>
2019-11-01 16:14:38 -0500

    fixed card view for projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,15 +1,33 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, {Component} from "react"
+import Link from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 import SiteNavbar from "../components/navbar"
+import {Card, Button} from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.css';
 
 
-const ProjectPage = () => (
-  <SiteNavbar>
-  </SiteNavbar>
-)
+class ProjectPage extends React.Component {
+  render(){
+    return (
+      <React.Fragment>
+          <SiteNavbar>
+          </SiteNavbar>
+          <Card style={{width: '18rem'}}>
+            <Card.ImgOverlay variant="top" src="gatsby-icon.png"/>
+            <Card.Body>
+              <Card.Title>Credit Card Fraud</Card.Title>
+              <Card.Text>
+                Placeholder text for test project card.
+              </Card.Text>
+              <Button variant="dark">View</Button>
+            </Card.Body>
+          </Card>
+      </React.Fragment>
+    )
+  }
+}
 
 export default ProjectPage
