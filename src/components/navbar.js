47684a9b3034c9e7c591268e4f802a894b3e9384
Tejus <tgmathew11@gmail.com>
2019-12-24 01:58:45 -0600

    added links, functionality to homepage

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,15 @@
 
 import React from "react"
 import {Navbar, Nav} from "react-bootstrap"
+import Helmet from "react-helmet"
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default () => 
   <Navbar bg='dark' variant='dark' style={{height: '10vh', width: '100%'}}>
+    <Helmet>
+        <meta charSet="utf-8" />
+        <title>Tejus Mathew</title>
+    </Helmet>
     <Navbar.Brand href="/">Tejus Mathew</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
