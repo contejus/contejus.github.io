ad55998e87d01cac8c049fc31bdac8c98362f66b
Tejus <tgmathew11@gmail.com>
2019-12-26 04:23:37 -0600

    updated to include prev locations

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -27,7 +27,8 @@ class SimpleMap extends Component {
       lng: 0
     },
     zoom: 0,
-    key: 'AIzaSyCQuo1OKiBqgURMSaSFpcxSrwMHjgCuLag'
+    key: 'AIzaSyCQuo1OKiBqgURMSaSFpcxSrwMHjgCuLag',
+    items: []
   };
 
   constructor() {
@@ -36,22 +37,57 @@ class SimpleMap extends Component {
         latitude: [],
         longitude: [],
         location: [],
+        prevLocations: [],
         done: false
     }
   }
 
   componentDidMount() {
-    fetch("https://json.geoiplookup.io")
-      .then(res => res.json())
-      .then(result => this.setState({
-        latitude: result.latitude,
-        longitude: result.longitude,
-        location: result.city + ", " + result.region + ", " + result.country_name,
-        done: true
-      })) 
+    fetch("https://infinite-ocean-46338.herokuapp.com/api/location", {
+      method: "GET",    
+    }).then(res => res.json())
+    .then(result2 => {
+      console.log(result2);
+      this.setState({
+        prevLocations: result2['objects'],
+      });
+      this.state.prevLocations.forEach ( location => {
+        console.log(location)
+        this.props.items.push(
+          <AnyReactComponent
+                lat={location['latitude']}
+                lng={location['longitude']}
+                text="Anon"
+            />
+        )
+      });
+      fetch("https://json.geoiplookup.io")
+        .then(res => res.json())
+        .then(result => {
+          this.setState({
+            latitude: result.latitude,
+            longitude: result.longitude,
+            location: result.city + ", " + result.region + ", " + result.country_name,
+            done: true
+          });
+          fetch("https://infinite-ocean-46338.herokuapp.com/api/location/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              "latitude": (this.state.latitude),
+              "longitude": (this.state.longitude)
+            })      
+          })
+          
+        })  
+      }  
+    )
   }
 
   render() {
+
     return (
       // Important! Always set the container height explicitly
       <React.Fragment>
@@ -65,11 +101,12 @@ class SimpleMap extends Component {
             defaultCenter={this.props.center}
             defaultZoom={this.props.zoom}
           >
+            {this.props.items}
              <AnyReactComponent
               lat={this.state.latitude}
               lng={this.state.longitude}
               text="You"
-            />
+              />
           </GoogleMapReact>
         </div>
         <div style={{position: 'absolute', bottom: '5%', left: '25%', right: '25%', textAlign: 'center'}}>
