e3d168905600e44cc36039dc5bf644f660adf7b0
contejus <22385070+contejus@users.noreply.github.com>
2020-06-07 16:21:17 -0500

    improve league layout on mobile

--- src/pages/league.js
+++ src/pages/league.js
@@ -15,9 +15,9 @@ class LeaguePage extends React.Component {
         return (
         <Layout>
             <Jumbotron 
-                title="League Stats" 
+                title="League of Legends Stats" 
                 fullscreen="" 
-                subtitle="Check out League of Legends matches I've played"
+                subtitle="Using the Riot API to access my match history"
             />
             <section className="section">
                 <StaticQuery
@@ -70,32 +70,44 @@ class LeaguePage extends React.Component {
                                                 const match = matchNode.node;
                                                 return (
                                                     // TODO: Add component here
-                                                    <article className={`leagueTile tile is-child is-12 ${match.win? ' has-background-success' : 'has-background-danger'}`}>
-                                                        <div class="leagueContent content">
-                                                            <div class="leagueContent">
-                                                                <p class="leagueTitle">{`${match.win? 'Win' : 'Loss'}`}</p>
+                                                    <article className={`allPadding tile is-child ${match.win? ' has-background-success' : 'has-background-danger'}`}>
+                                                        <div class="content matchStat columns">
+                                                            <div class="column is-4 is-full-mobile">
+                                                                <div class="columns is-mobile">
+                                                                    <div class="column is-6 is-flex centerFlex">
+                                                                        <p class="winLoss">{`${match.win? 'Win' : 'Loss'}`}</p>
+                                                                    </div>
+                                                                    <div class="column is-6 is-flex centerFlex wrap">
+                                                                        <figure className="image is-256x256 champImg is-flex centerFlex">
+                                                                            <img className="is-rounded" src={championUrl + match.champion + ".png"} />
+                                                                        </figure>
+                                                                        <p class="leagueTitle">{match.champion}</p>
+                                                                    </div>
+                                                                </div>
                                                             </div>
-                                                            <div class="pb2pt2 flexCC">
-                                                                <figure className="champImg image is-96x96">
-                                                                    <img className="is-rounded" src={championUrl + match.champion + ".png"} />
-                                                                </figure>
-                                                                <p class="leagueTitle">{match.champion}</p>
+                                                            <div class="column is-4 is-full-mobile">
+                                                                <div class="columns is-mobile">
+                                                                    <div class="column is-6">
+                                                                        <p class="leagueTitle">K/D/A</p>
+                                                                        <p class="leagueSubtitle">{match.kda}</p>
+                                                                    </div>
+                                                                    <div class="column is-6">
+                                                                        <p class="leagueTitle">Map</p>
+                                                                        <p class="leagueSubtitle">{match.map}</p>
+                                                                    </div>
+                                                                </div>
                                                             </div>
-                                                            <div class="pb2pt2">
-                                                                <p class="leagueTitle">K/D/A</p>
-                                                                <p class="leagueSubtitle">{match.kda}</p>
-                                                            </div>
-                                                            <div class="pb2pt2">
-                                                                <p class="leagueTitle">Map</p>
-                                                                <p class="leagueSubtitle">{match.map}</p>
-                                                            </div>
-                                                            <div class="pb2pt2">
-                                                                <p class="leagueTitle">Game Type</p>
-                                                                <p class="leagueSubtitle">{match.gameType}</p>
-                                                            </div>
-                                                            <div class="pb2pt2">
-                                                                <p class="leagueTitle">Game Length</p>
-                                                                <p class="leagueSubtitle">{String(Math.floor(match.length / 60)) + ":" + String('0' + Math.floor(match.length % 60)).slice(-2)}</p>
+                                                            <div class="column is-4 is-full-mobile">
+                                                                <div class="columns is-mobile">
+                                                                    <div class="column is-6">
+                                                                        <p class="leagueTitle">Game Type</p>
+                                                                        <p class="leagueSubtitle">{match.gameType}</p>
+                                                                    </div>
+                                                                    <div class="column is-6">
+                                                                        <p class="leagueTitle">Game Length</p>
+                                                                        <p class="leagueSubtitle">{String(Math.floor(match.length / 60)) + ":" + String('0' + Math.floor(match.length % 60)).slice(-2)}</p>
+                                                                    </div>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </article>
